a756cbbdb34f5dcb2f09b60b71e7cda7decf28d5
xusong <595304523@qq.com>
2018-02-25 17:36:14 +0800

    more notes

---
+++ src/main/java/chapter05/caseX/Cup.java
@@ -0,0 +1,16 @@
+package chapter05.caseX;
+
+/**
+ * Created by xusong on 2018/2/25.
+ */
+public class Cup {
+    Cup(int marker){
+        System.out.println("Cup(" + marker + ")");
+    }
+
+    void f(int marker){
+        System.out.println("f(" + marker + ")");
+    }
+
+
+}
---
+++ src/main/java/chapter05/caseX/Spoon.java
@@ -0,0 +1,12 @@
+package chapter05.caseX;
+
+/**
+ * 静态块
+ */
+public class Spoon {
+    static int i;
+    // 静态块
+    static {
+        i = 47;
+    }
+}
