a3293dbd63fde4191dc3e4ec6b6c31eb04d29a2c
LucasXuS <595304523@qq.com>
2018-03-02 13:54:21 +0800

    note more on mar 2nd 2018

---
+++ src/main/java/chapter08/case07/Meal.java
@@ -0,0 +1,12 @@
+package chapter08.case07;
+
+import static util.Print.print;
+
+/**
+ * Created by lenovo on 2018/3/2.
+ */
+public class Meal {
+    Meal(){
+        print("Meal()");
+    }
+}
--- src/main/java/chapter08/case07/Sandwich.java
+++ src/main/java/chapter08/case07/Sandwich.java
@@ -1,7 +1,13 @@
 package chapter08.case07;
-
+import static util.Print.print;
 /**
- * Created by lenovo on 2018/3/2.
+ * 构造器和多态
+ * “上溯造型”给了我们一个解释为何派生类构造之前要确保基类运行构造函数。
+ * 由于在上溯造型的时候，我们不仅需要了解该类型的全部，也要同时了解其基类
+ * 的全部。
  */
-public class Sandwich {
+public class Sandwich extends PortableLunch {
+    Sandwich(){
+        print("Sandwich()");
+    }
 }
