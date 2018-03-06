b2ac2c49acfcbabf3680ea1067bc5b33fa062337
LucasXuS <595304523@qq.com>
2018-03-06 13:04:37 +0800

    note more on mar 2nd 2018

--- src/main/java/chapter09/case10/Games.java
+++ src/main/java/chapter09/case10/Games.java
@@ -8,7 +8,6 @@ public class Games {
     public static void playGame(final GameFactory gameFactory, final int playerCount) {
 
         // 在服务器中，每次添加一个请求，我们就会生成一个新线程，让一个新玩家参与游戏
-
         for (int i = 0; i < playerCount; i++) {
             Thread thread = new Thread(new Runnable() {
                 public void run() {
