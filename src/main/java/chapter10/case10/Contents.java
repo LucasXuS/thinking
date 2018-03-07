e1695f30e01c7c2be1bd056b777d719434ae832b
LucasXuS <595304523@qq.com>
2018-03-07 13:59:26 +0800

    note more on mar 2nd 2018

---
+++ src/main/java/chapter10/case10/Contents.java
@@ -0,0 +1,8 @@
+package chapter10.case10;
+
+/**
+ * Created by lenovo on 2018/3/6.
+ */
+public interface Contents {
+    int value();
+}
--- src/main/java/chapter10/case10/Parcel11.java
+++ src/main/java/chapter10/case10/Parcel11.java
@@ -2,7 +2,7 @@ package chapter10.case10;
 
 /**
  * 嵌套类：
- *
+ * <p>
  * 如果想阻断内部类对象和外围类对象的联系，可以设置内部类为static，
  * 我们通常称之为嵌套类。
  * 另外：
@@ -10,4 +10,55 @@ package chapter10.case10;
  * 2 不能从嵌套类的对象中访问非静态外围类对象。
  */
 public class Parcel11 {
+
+    private static class ParcelContents implements Contents {
+
+        private int i = 11;
+
+        public int value() {
+            return i;
+        }
+    }
+
+    static class ParcelDestination implements Destination {
+
+        private String label;
+
+        public ParcelDestination(String whereTo) {
+            this.label = whereTo;
+        }
+
+        public String readLabel() {
+            return label;
+        }
+
+        public static void f() {
+        }
+
+        static int x = 10;
+
+        static class AnotherLevel {
+            public static void f() {
+            }
+
+            static int x = -10;
+        }
+    }
+
+    public static Destination destination(String s){
+        return new ParcelDestination(s);
+    }
+
+    public static Contents contents(){
+        return new ParcelContents();
+    }
+
+    public static void main(String[] args){
+        Contents c = contents();
+
+        Destination d = destination("Tasmania");
+    }
+
+
+
 }
