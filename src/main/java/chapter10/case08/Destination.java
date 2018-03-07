2f50b7cd1329ea1e29e83d2d3a02f7211dc35b3e
LucasXuS <595304523@qq.com>
2018-03-07 11:17:07 +0800

    note more on mar 2nd 2018

---
+++ src/main/java/chapter10/case08/Destination.java
@@ -0,0 +1,8 @@
+package chapter10.case08;
+
+/**
+ * Created by lenovo on 2018/3/6.
+ */
+public interface Destination {
+    String readLabel();
+}
---
+++ src/main/java/chapter10/case08/Parcel10.java
@@ -0,0 +1,31 @@
+package chapter10.case08;
+
+/**
+ * Created by lenovo on 2018/3/7.
+ */
+public class Parcel10 {
+    public Destination destination(final String dest, final float price) {
+        return new Destination() {
+
+            private int cost;
+
+            {
+                cost = Math.round(price);
+                if (cost > 100) {
+                    System.out.println("Over budget!");
+                }
+            }
+
+            private String label = dest;
+
+            public String readLabel() {
+                return label;
+            }
+        };
+    }
+
+    public static void main(String[] args){
+        Parcel10 p = new Parcel10();
+        Destination d = p.destination("Tasmania", 101.395f);
+    }
+}
