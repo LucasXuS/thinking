1bb094103c5063a266439f314e6c84a245a6bf82
LucasXuS <595304523@qq.com>
2018-03-09 16:47:27 +0800

    note more on mar 2nd 2018

--- src/main/java/chapter10/case15/Callee2.java
+++ src/main/java/chapter10/case15/Callee2.java
@@ -22,6 +22,16 @@ public class Callee2 extends MyIncrement {
     }
 
     Incrementable getCallbackReference() {
+
+        /*
+        * Closure 类 类似于指针，我们可以通过它去访问和修改Callee2
+        * 在这里我们发现两个特性：
+        * Closure 依靠外围类对象去操纵Callee2
+        * 我们可以可控地去控制Closure的功能，不像指针，可以让客户端
+        * 程序员为所欲为。因此，这个思路是类似于指针，但是比指针安全
+        * 得多的策略。
+        * **/
+        // 这是一个操纵Callee2的钩子
         return new Closure();
     }
 }
---
+++ src/main/java/chapter10/case16/GreenhouseControls.java
@@ -0,0 +1,7 @@
+package chapter10.case16;
+
+/**
+ * Created by lenovo on 2018/3/9.
+ */
+public class GreenhouseControls extends Controller {
+}
