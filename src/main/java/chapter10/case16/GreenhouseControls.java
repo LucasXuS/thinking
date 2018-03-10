a5fb00d834dddbceccb78c637e0fc1a40493ed5f
LucasXuS <595304523@qq.com>
2018-03-10 15:38:20 +0800

    note more on mar 2nd 2018

--- src/main/java/chapter10/case16/GreenhouseControls.java
+++ src/main/java/chapter10/case16/GreenhouseControls.java
@@ -1,7 +1,48 @@
 package chapter10.case16;
 
 /**
- * Created by lenovo on 2018/3/9.
+ * 我们这里是想把变化的事物和不变的事物相分离：
+ *
+ *
  */
 public class GreenhouseControls extends Controller {
+    private boolean light = false;
+
+    public class LightOn extends Event {
+
+        public LightOn(long delayTime) {
+            super(delayTime);
+        }
+
+        @Override
+        public void action() {
+            light = true;
+        }
+
+        @Override
+        public String toString() {
+            return "Light is on";
+        }
+    }
+
+    public class LightOff extends Event {
+
+        public LightOff(long delayTime) {
+            super(delayTime);
+        }
+
+        @Override
+        public void action() {
+            light = false;
+        }
+
+        @Override
+        public String toString() {
+            return "Light is off";
+        }
+    }
+
+    private boolean water = false;
+
+
 }
