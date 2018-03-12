90432477c7041ad1427805b9da7163665d7fea26
xusong <595304523@qq.com>
2018-03-12 22:42:01 +0800

    more notes

--- src/main/java/chapter11/case06/LinkedListFeatures.java
+++ src/main/java/chapter11/case06/LinkedListFeatures.java
@@ -1,9 +1,13 @@
 package chapter11.case06;
 
+import chapter14.caseX.Hamster;
 import chapter14.caseX.Pet;
+import chapter14.caseX.Rat;
 import chapter14.caseY.Pets;
 
 import java.util.LinkedList;
+
+import static util.Print.print;
 
 /**
  * LinkedList 和ArrayList 相比，在插入和删除元素中开销更小，
@@ -14,6 +18,45 @@ import java.util.LinkedList;
 public class LinkedListFeatures {
     public static void main(String[] args) {
         LinkedList<Pet> pets = new LinkedList<>(Pets.arrayList(5));
+        print(pets);
+
+        // getFirst() element() peek()的功能完全一致，都是取第一个元素，
+        // 但是当列表为空时，getFirst() element()抛出 NoSuchElementException
+        // peek()直接返回null
+        print("pets.getFirst(): " + pets.getFirst());
+        print("pets.element(): " + pets.element());
+        print("pets.peek(): " + pets.peek());
+
+        // removeFirst() remove() poll()的功能完全一致，都是删除第一个元素，并且返回被删除的元素，
+        // 但是当列表为空时，removeFirst() remove()抛出 NoSuchElementException
+        // poll()直接返回null
+        print("pets.remove(): " + pets.remove());
+        print("pets.removeFirst(): " + pets.removeFirst());
+        print("pets.poll(): " + pets.poll());
+
+        print(pets);
+
+        // 在列表头部添加一个元素
+        pets.addFirst(new Rat());
+        print("After addFirst(): " + pets);
+
+
+        // offer() add() addLast()功能相同，都是在列表尾部添加元素
+        pets.offer(Pets.randomPet());
+        print("After offer(): " + pets);
+
+        pets.add(Pets.randomPet());
+        print("After add(): " + pets);
+
+
+        pets.addLast(new Hamster());
+        print("After addLast(): " + pets);
+
+        // 删除最后一个元素，并且返回被删除的元素
+        print("pets.removeLast(): " + pets.removeLast());
+
+
+
 
     }
 }
