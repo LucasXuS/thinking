1096ecfe959f144d1cdab6955234f537dcd6e66c
LucasXuS <595304523@qq.com>
2018-03-12 13:42:20 +0800

    note more on mar 2nd 2018

--- src/main/java/chapter11/case03/ListFeatures.java
+++ src/main/java/chapter11/case03/ListFeatures.java
@@ -80,6 +80,42 @@ public class ListFeatures {
         copy.retainAll(sub);
         print("13: " + copy);
 
+        // 单个删除和批量删除
+        copy = new ArrayList<>(pets);
+        copy.remove(2);
+        print("14: " + copy);
+        copy.retainAll(sub);
+        print("15: " + copy);
+
+        // 修改元素
+        copy.set(1, new Mouse());
+        print("16: " + copy);
+
+        // 在指定位置批量添加文件
+        copy.addAll(2, sub);
+        print("17: " + copy);
+
+        // 判断列表是否为空
+        print("18: " + pets.isEmpty());
+
+        // 清空列表
+        pets.clear();
+        print("19: " + pets);
+        print("20: " + pets.isEmpty());
+
+        pets.addAll(Pets.arrayList(4));
+        print("21: " + pets);
+
+        // 列表转数组
+        Object[] o = pets.toArray();
+        print("22: " + o[3]);
+
+        // toArray()返回的是Object[]，为了返回其他类型，一定要加个参数
+        // 生成的Pet数组的长度，是new Pet[x] 中的x与pets.size()里面比较大的一个。
+        Pet[] pa = pets.toArray(new Pet[0]);
+        print("length of pa: " + pa.length);
+        print("22: " + pa[3].id());
+
 
     }
 }
