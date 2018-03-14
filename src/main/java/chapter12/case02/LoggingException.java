41665f798631232cacea4b646c1c50f9ee5583a9
LucasXuS <595304523@qq.com>
2018-03-14 09:45:44 +0800

    note more on mar 14th 2018

---
+++ src/main/java/chapter12/case02/LoggingException.java
@@ -0,0 +1,20 @@
+package chapter12.case02;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.logging.Logger;
+
+/**
+ * Created by lenovo on 2018/3/14.
+ */
+public class LoggingException extends Exception {
+
+    private static Logger logger =
+            Logger.getLogger("LoggingException");
+
+    public LoggingException(){
+        StringWriter trace = new StringWriter();
+        printStackTrace(new PrintWriter(trace));
+        logger.severe(trace.toString());
+    }
+}
