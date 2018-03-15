5ffa441d98ec73ec918ca2e6eec3d3d3f4e3d48b
LucasXuS <595304523@qq.com>
2018-03-15 17:34:53 +0800

    note more on mar 15th 2018

--- src/main/java/chapter12/case05/DynamicFields.java
+++ src/main/java/chapter12/case05/DynamicFields.java
@@ -79,4 +79,22 @@ public class DynamicFields {
         return result;
     }
 
+    public static void main(String[] args) {
+        DynamicFields df = new DynamicFields(3);
+        System.out.println(df);
+        try {
+            df.setField("d", "A value for d");
+            df.setField("number", 47);
+            df.setField("number2", 48);
+            System.out.println(df);
+            df.setField("d", "A new value for d");
+            df.setField("number3", 11);
+            System.out.println("df: " + df);
+            System.out.println("df.getField(\"d\"): " + df.getField("d"));
+            Object field = df.setField("d", null);
+        } catch (DynamicFieldsException | NoSuchFieldException e) {
+            e.printStackTrace(System.out);
+        }
+    }
+
 }
