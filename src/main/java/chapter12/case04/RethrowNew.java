356b11d37032d1de988b4bcef00d933352b6109f
LucasXuS <595304523@qq.com>
2018-03-15 17:21:34 +0800

    note more on mar 15th 2018

--- src/main/java/chapter12/case04/RethrowNew.java
+++ src/main/java/chapter12/case04/RethrowNew.java
@@ -5,4 +5,25 @@ package chapter12.case04;
  * 重新抛出新exception时可以让原异常的信息都一并丢失。
  */
 public class RethrowNew {
+    public static void f() throws OneException {
+        System.out.println("originating the exception in f()");
+        throw new OneException("thrown from f()");
+    }
+
+    public static void main(String[] args){
+        try {
+            try {
+                f();
+            }catch (OneException e){
+                System.out.println("Caught in inner try, e.printStackTrace()");
+                e.printStackTrace(System.out);
+                // 在catch中处理元错误，抛出新错误，两个异常之间没有联系。
+                throw new TwoException("from inner try");
+            }
+        }catch (TwoException e){
+            System.out.println("Caught in outer try, e.printStackTrace()");
+            e.printStackTrace(System.out);
+        }
+    }
+
 }
---
+++ src/main/java/chapter12/case05/DynamicFieldsException.java
@@ -0,0 +1,7 @@
+package chapter12.case05;
+
+/**
+ * Created by lenovo on 2018/3/15.
+ */
+public class DynamicFieldsException extends Exception {
+}
