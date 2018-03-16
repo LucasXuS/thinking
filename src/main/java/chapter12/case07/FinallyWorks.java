057a6c34ebd21994bf81932583dce009e9f2791f
LucasXuS <595304523@qq.com>
2018-03-16 09:59:56 +0800

    note more on mar 15th 2018

--- src/main/java/chapter12/case07/FinallyWorks.java
+++ src/main/java/chapter12/case07/FinallyWorks.java
@@ -1,9 +1,11 @@
 package chapter12.case07;
 
 /**
- * Created by lenovo on 2018/3/16.
+ * 利用finally 实现程序的多次尝试机制
  */
 public class FinallyWorks {
+
+    static int count = 0;
 
     public static void main(String[] args){
 
