f746cf4e0aec689e1edac729740c625ccaadcaee
xusong <595304523@qq.com>
2018-03-18 18:57:19 +0800

    more notes on Mar 18th

--- src/main/java/chapter15/caseX/CoffeeGenerator.java
+++ src/main/java/chapter15/caseX/CoffeeGenerator.java
@@ -1,26 +1,66 @@
 package chapter15.caseX;
 
 import java.util.Iterator;
+import java.util.Random;
 
 /**
  * Created by xusong on 2018/3/18.
  */
 public class CoffeeGenerator implements Generator<Coffee>, Iterable<Coffee> {
+
+    private Class[] types = {Latte.class, Mocha.class, Cappuccino.class, Americano.class, Breve.class};
+
+    private static Random random = new Random(47);
+
+    public CoffeeGenerator() {
+    }
+
+    private int size = 0;
+
+    public CoffeeGenerator(int sz) {
+        size = sz;
+    }
+
     @Override
     public Coffee next() {
-        return null;
+        try {
+            return (Coffee) types[random.nextInt(types.length)].newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Iterator<Coffee> iterator() {
-        return null;
+        return new CoffeeIterator();
     }
 
-    class CoffeeIterator implements Iterable<Coffee> {
+    class CoffeeIterator implements Iterator<Coffee> {
+
+        int count = size;
 
         @Override
-        public Iterator<Coffee> iterator() {
-            return null;
+        public boolean hasNext() {
+            return count > 0;
+        }
+
+        @Override
+        public Coffee next() {
+            count--;
+            return CoffeeGenerator.this.next();
+        }
+
+        public void removed() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    public static void main(String[] args) {
+        CoffeeGenerator generator = new CoffeeGenerator();
+        for (int i = 0; i < 5; i++)
+            System.out.println(generator.next());
+        for (Coffee c : new CoffeeGenerator(5)) {
+            System.out.println(c);
         }
     }
 }
