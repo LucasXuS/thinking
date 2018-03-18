004a0024b58e47337eb2d3f1e1d5421d4084d1bd
xusong <595304523@qq.com>
2018-03-18 13:58:53 +0800

    more notes on Mar 18th

---
+++ src/main/java/chapter13/case01/UsingStringBuilder.java
@@ -0,0 +1,46 @@
+package chapter13.case01;
+
+/**
+ * 字符串拼接的机理：
+ * 字符串直接相加，实际上编译器在内部调用了StringBuilder,StringBuilder是一个非常高效的拼接字符串的方案
+ *
+ * StringBuilder拼接字符串机理：
+ * StringBuilder之所以高效，原理如下：
+ * 和C一样，Java的字符串是基于字符数组。调用append后，位于StringBuilder内部的字符数组value
+ * 先扩充size,然后将新的数组衔接到老的数组里面去。
+ *
+ * 从内存的角度来看，每次扩充是要重新构建字符数组，所以要消耗一定内存
+ * 从运算角度看，字符串拼接的关键方法Arrays.copy()是个native方法，意味着这是一个由C语言编写的方法，速度
+ * 比Java方法更快，
+ *
+ * 所以调用append方法后，并不会产生新的String对象，但是使用stringBuilder.toString()才会把内部的char[] value
+ * 转换为String
+ *
+ *
+ *
+ */
+public class UsingStringBuilder {
+
+
+    // base1 和 base2的代码完全一样，包括内部机理，在这种情况下，IntelliJ IDEA推荐base1,因为可读性更好。
+
+    public String base1() {
+        String stringBuilder = "My" +
+                " name" +
+                " is" +
+                " Gordon";
+        return stringBuilder;
+    }
+
+    public String base2() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("My");
+        stringBuilder.append(" name");
+        stringBuilder.append(" is");
+        stringBuilder.append(" Gordon");
+        return stringBuilder.toString();
+    }
+
+
+
+}
